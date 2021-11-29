4d859e5ad7e38c7e6bfed5c166802b2aee37070e
Peter Nicolas McDaniel <kg.mcd@hotmail.com>
2021-11-29 16:12:42 +0100

    update

--- lib/providers/jira_provider.dart
+++ lib/providers/jira_provider.dart
@@ -1,9 +1,15 @@
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:jira_tracker/models/jira_case.dart';
 import 'package:jira_tracker/services/jira.dart';
 
-class JiraStateNotifier {
+class JiraStateNotifier extends StateNotifier<List<JiraCase>> {
   final JiraService _jiraService = JiraService();
-
+  JiraStateNotifier() : super(const []);
   Future<List<JiraCase>> get getJiraCases =>
       _jiraService.getTaskAssignedToUser(1);
 }
+
+final jiraStateProvider =
+    StateNotifierProvider<JiraStateNotifier, List<JiraCase>>((ref) {
+  return JiraStateNotifier();
+});
---
+++ lib/widgets/track_list_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class TrackListView extends StatelessWidget {
+  const TrackListView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
